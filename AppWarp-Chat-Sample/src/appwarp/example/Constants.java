f90b8140664e9ac786ea622e8d115d5f0855a007
Vishnu <vishnu.garg@shphertz.co.in>
2016-09-15 11:35:50 +0530

    Samples Updated
    Signed-off-by: Vishnu <vishnu.garg@shphertz.co.in>

--- AppWarp-Chat-Sample/src/appwarp/example/Constants.java
+++ AppWarp-Chat-Sample/src/appwarp/example/Constants.java
@@ -3,9 +3,9 @@ package appwarp.example;
 public class Constants {
 
 	// Sample API Key & Secret Key  
-	
+	   public static final String roomId = "1469583531"; // static room id
 	public static String apiKey = "Your API Key";
     public static String secretKey =  "Your Secret Key";
-    public static final String roomId = "1469583531"; // static room id
+ 
 
 }
--- AppWarpUDPDemo/src/com/app/appwarpudpdemo/Constants.java
+++ AppWarpUDPDemo/src/com/app/appwarpudpdemo/Constants.java
@@ -6,8 +6,7 @@ public class Constants {
 	 *  Please replace these keys with you API Key and Secret Keys 
 	 */
 
-	public static String apiKey = "Your AppWarp API Key";
-    public static String secretKey =  "Your AppWarp Secret Key";
+
     
     public static final int BananaId = 1;
     public static final int GrapeId = 2;
@@ -15,5 +14,8 @@ public class Constants {
     public static final int StrawaryId = 4;
     
     public static final int MonsterSpeed = 400;	// px per second
+    
+	public static String apiKey = "Your AppWarp API Key";
+    public static String secretKey =  "Your AppWarp Secret Key";
     
 }
